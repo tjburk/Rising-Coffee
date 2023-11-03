04d06b3fb3fd9cacb2ce3768e4328a383db5efea
tjburk <ty_burkhardt@verizon.net>
2023-11-03 18:53:35 -0400

    added whitenoise middleware

--- rising_coffee/settings.py
+++ rising_coffee/settings.py
@@ -52,6 +52,7 @@ ROOT_URLCONF = 'rising_coffee.urls'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
